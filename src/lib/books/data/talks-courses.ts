d35addc6ebf71464100c49fb9b7e3ba589e4e688
andytudhope <13001517+andytudhope@users.noreply.github.com>
2025-01-30 15:28:00 +0200

    walk the talk

--- src/lib/books/data/talks-courses.ts
+++ src/lib/books/data/talks-courses.ts
@@ -35,12 +35,63 @@ export const talksCoursesBooks: Book[] = [
         samplePdfUrl: "/samples/FulfilmentNow.pdf",
       },
       {
-        id: "seasons-of-wisdom",
-        title: "Seasons Of Wisdom",
-        description: " Seasons of Wisdom is mostly collated from the closing unscripted talks given by Shaykh Fadhlalla Haeri to seal the annual conferences held by the Academy of Self Knowledge in South Africa. It was his love for Qur'an that brought about a situation of regularly sharing commentaries and delights in the eternal Truth that whatever is known and unknown emanates from one Cosmic Source that is not subject to space or time.\n\n<i>\"Every living entity is a combination of a mysterious divine spirit or soul, which provides life and is eternal and boundless in nature, and a physical identity which connects the divine attributes of the soul with all earthly, transient realities and shadows. With intelligence and faith you will realize how these descriptions will provide seeds of prescriptions. If you dwell on either, you realise they are two facets of the same reality. One describes and the other leads to responsibility and drive. From the inert to the dynamic and moving.</i>\"\n\n- Shaykh Fadhlalla Haeri",
-        coverImage: "/images/book-covers/SeasonsOfWisdom.jpg",
+        id: "sufi-map-self",
+        title: "ASK Course ONE: The Sufi Map of the Self",
+        description: " This book consists of all 14 lessons of the Academy of Self Knowledge's <b><i>Course ONE -- Cosmology of the Self</i></b>. It has been renamed to <b><i>The Sufi Map of the Self</i></b>.\n\nThe book explores the entire cosmology of the self through time and maps the evolution of the self from before birth through life, death and beyond.\n\nThe course is contextualized within the age we are living in and is aimed at anybody with a yearning for higher knowledge based on eternal truth. It shows us how to attain greater levels of inner stability in an unstable world so that we can deal with, and even appreciate the outer instability we find ourselves in, rather than constantly resisting it.\n\nThe course takes us a step at a time towards the realization that we are outwardly limited but inwardly far more than who we thought we were. It shows us how to prioritize everything that is sacred and enduring in our lives and to discard all that casts us far from the One. The book gives us the means by which we can nurture this higher aspect within ourselves in a wholesome way, making it an invaluable guide to life.",
+        coverImage: "/images/book-covers/ASKCourseOne.jpg",
         category: "talks-courses",
         order: 2,
+        publishedDate: new Date('2021-11-07'),
+        purchaseLinks: {
+          amazon: "1919826289",
+          kindle: "B01JD6CO82",
+          kobo: "http://store.kobobooks.com/en-CA/ebook/the-sufi-map-of-the-self",
+          apple: "http://itunes.apple.com/us/book/id1145048135",
+          other: [
+            {
+              name: "Waterstones",
+              url: "https://www.waterstones.com/book/the-sufi-map-of-the-self/shaykh-fadhlalla-haeri/9781919826288",
+              geography: "UK",
+            },
+          ]
+        },
+        samplePdfUrl: "/samples/TheSufiMapOfTheSelf.pdf",
+      },
+      {
+        id: "prophetic-way-of-life",
+        title: "ASK Course TWO: The Prophetic Way of Life",
+        description: " This book consists of all 12 lessons of the Academy of Self Knowledge's <b><i>Course TWO -- The Prophetic Revealed Path</i></b>. It has been renamed to <b><i>The Prophetic Way of Life</i></b>.\n\n After discovering the map of the self, we learn here how the code of ethics that govern religious practice and the Prophetic way are in fact transformational tools to enlightened awakening. Based on the cosmology/view that the heart is the connecting point between the soul (the eternal), and the self (the ephemeral), the course focuses on transformation through awareness and following the practices of the Prophetic path.\n\nThis way of transformation includes being accountable and responsible while being continually aware of the soul and higher consciousness. We learn that we must continually strive for perfection but with the knowledge that no one attains such perfection in an enduring way and that it is God's perfect attributes that permeate all of existence and bind, unify and drive creation towards fulfillment.\n\nThe objective of this course is to enable the seeker to love the One unconditionally and deal with multiplicity rationally. ",
+        coverImage: "/images/book-covers/ASKCourseTwo.jpg",
+        category: "talks-courses",
+        order: 3,
+        publishedDate: new Date('2021-11-10'),
+        purchaseLinks: {
+          amazon: "1919826297",
+          kindle: "B01JD72Q3Y",
+          kobo: "http://store.kobobooks.com/en-CA/ebook/the-prophetic-way-of-life",
+          apple: "http://itunes.apple.com/us/book/id1145039121",
+          other: [
+            {
+                name: "Barnes & Noble",
+                url: "https://www.barnesandnoble.com/w/the-prophetic-way-of-life-shaykh-fadhlalla-haeri/1140784834",
+                geography: "US",
+            },
+            {
+              name: "Waterstones",
+              url: "https://www.waterstones.com/book/the-prophetic-way-of-life/shaykh-fadhlalla-haeri/9781919826295",
+              geography: "UK",
+            },
+          ]
+        },
+        samplePdfUrl: "/samples/ThePropheticWayOfLife.pdf",
+      },
+      {
+        id: "seasons-of-wisdom",
+        title: "Seasons Of Wisdom",
+        description: "Seasons of Wisdom is mostly collated from the closing unscripted talks given by Shaykh Fadhlalla Haeri to seal the annual conferences held by the Academy of Self Knowledge in South Africa. It was his love for Qur'an that brought about a situation of regularly sharing commentaries and delights in the eternal Truth that whatever is known and unknown emanates from one Cosmic Source that is not subject to space or time.\n\n<i>\"Every living entity is a combination of a mysterious divine spirit or soul, which provides life and is eternal and boundless in nature, and a physical identity which connects the divine attributes of the soul with all earthly, transient realities and shadows. With intelligence and faith you will realize how these descriptions will provide seeds of prescriptions. If you dwell on either, you realise they are two facets of the same reality. One describes and the other leads to responsibility and drive. From the inert to the dynamic and moving.</i>\"\n\n- Shaykh Fadhlalla Haeri",
+        coverImage: "/images/book-covers/SeasonsOfWisdom.jpg",
+        category: "talks-courses",
+        order: 4,
         publishedDate: new Date('2023-11-19'),
         purchaseLinks: {
           amazon: "1928329454",
@@ -56,5 +107,149 @@ export const talksCoursesBooks: Book[] = [
           ]
         },
         samplePdfUrl: "/samples/SeasonsOfWisdom-Sample.pdf",
+      },
+      {
+        id: "refinement-of-character",
+        title: "Refinement of Character: Friday Discourses",
+        description: "\"<i>How do we transform ourselves using the spiritual map from our <b>Deen</b>? How do we move through the four stages of inner awakening from <b>`Ilma Al Yaqin</b> (the Knowledge of Certainty) to <b>`Ayn Al-Yaqin</b> (the Eye of certainty) to <b>Haqq Al-Yaqin</b> (the Total Reality of Certainty) and its final stage, <b>Haqq Al-Haqq</b> (the Truth of Truth). What is the Qur'anic prescription of life, which allows the refinement of character from base to sublime?</i>\"\n\nThe '<i>Refinement of Character</i>' discourses offered by Shaykh Fadhlalla Haeri in South Africa present a comprehensive and complete package of essentials regarding the self and its emergence, of the interplay between the ego and the soul (behaviour and personality dynamics). This book presents these discourses allowing one to become closer to discovering the true answers for these questions.\n\n\"<i>Become emotional and you are disturbing the heart, which represents the access to your soul. Through the soul you perceive the appropriateness of knowledge.</i>\"",
+        coverImage: "/images/book-covers/RefinementOfCharacter.jpg",
+        category: "talks-courses",
+        order: 5,
+        publishedDate: new Date('2023-11-19'),
+        purchaseLinks: {
+          amazon: "1928329055",
+          kindle: "B0C5FR567B",
+          kobo: "https://www.kobo.com/ca/en/ebook/refinement-of-character-1",
+          apple: "http://books.apple.com/us/book/id6449231090",
+          other: [
+            {
+                name: "Barnes & Noble",
+                url: "https://www.barnesandnoble.com/w/refinement-of-character-shaykh-fadhlalla-haeri/1128116584",
+                geography: "US"
+            },
+            {
+              name: "Takealot",
+              url: "https://www.takealot.com/refinement-of-character-friday-discourses/PLID50555369",
+              geography: "ZA",
+            },
+            {
+                name: "Waterstones",
+                url: "https://www.waterstones.com/book/refinement-of-character/shaykh-fadhlalla-haeri/97819283290537",
+                geography: "UK"
+            }
+          ]
+        },
+      },
+      {
+        id: "friday-discourses-vol-1",
+        title: "Friday Discourses - Volume 1: Jum`ah Talks at the Rasooli Centre",
+        description: " These talks were delivered over a series of Friday prayer gatherings at the Rasooli Center, Centurion, south of Pretoria, South Africa.\n\nShaykh Fadhlalla Haeri boldly addresses many topics that influence Muslims at the core of what it means to be a Muslim in today's global village and how to be transformed by living the Deen of Islam truly.\n\nWith his finger on the pulse of our times, Shaykh Fadhlalla Haeri highlights the critical issues with a sense of urgency, clarity of insight and guidance grounded in the Qur'an and Prophetic way.\n\nThese challenges ultimately revolve around the core science of self-knowledge and awakening to the inner potential within us to live in harmony with God's decree of unity, justice and mercy.",
+        coverImage: "/images/book-covers/SeasonsOfWisdom.jpg",
+        category: "talks-courses",
+        order: 6,
+        publishedDate: new Date('2023-11-19'),
+        purchaseLinks: {
+          kindle: "B01JD9JBWG",
+          kobo: "http://store.kobobooks.com/en-CA/ebook/friday-discourses-volume-1",
+          apple: "http://itunes.apple.com/us/book/id1144952082",
+          other: [
+            {
+                name: "Barnes & Noble",
+                url: "https://www.barnesandnoble.com/w/friday-discourses-volume-1-shaykh-fadhlalla-haeri/1111793472",
+                geography: "US"
+            },
+            {
+                name: "Takealot",
+                url: "https://www.takealot.com/friday-discourses-volume-1/PLID92406432",
+                geography: "ZA",
+            },
+            {
+                name: "Waterstones",
+                url: "https://www.waterstones.com/book/friday-discourses/shaykh-fadhlalla-haeri/9781928329367",
+                geography: "UK"
+            }
+          ]
+        },
+        samplePdfUrl: "/samples/FridayDiscoursesVolume1.pdf",
+      },
+      {
+        id: "songs-of-iman-roads-pakistan",
+        title: "Songs of Iman On the Roads Of Pakistan",
+        description: "<b><i>Songs of Iman On the Roads Of Pakistan</i></b> is a series of talks given by Shaykh Fadhlalla Haeri during a tour of the country in 1982. Shaykh Fadhlalla speaks out on the central theme of the conflict between the pursuit of Western technologic and materialistic goals and a spiritual way of life promising fulfillment by seeking knowledge of God and living the Way of Submission, Islam. This divergence between <i>Iman</i> and <i>Kufr</i> taking place in Pakistan is only a reflection of a condition occurring everywhere else in the Muslim world, and indeed in the rest of the world today.\n\nThis volume of collected talks, which includes a detailed commentary on Surah al-Infitar (Surah 82), is relevant to anyone who is seeking a way out of the chaos of a collapsing, amoral Kufr society, and a clear guidance and knowledge of an alternative way of living -- a code of conduct that will enable him to travel with humility and purpose toward his Creator and the fulfillment of his noble destiny. ",
+        coverImage: "/images/book-covers/SongsOfImanOnTheRoadsOfPakistan.jpg",
+        category: "talks-courses",
+        order: 7,
+        purchaseLinks: {
+          kindle: "B01JDKL56K",
+          kobo: "http://store.kobobooks.com/en-CA/ebook/songs-of-iman-on-the-roads-of-pakistan",
+          apple: "http://itunes.apple.com/us/book/id1144985596",
+        },
+        samplePdfUrl: "/samples/SongsOfIman.pdf",
+      },
+      {
+        id: "connection-absolute-relative",
+        title: "The Connection Between the Absolute and the Relative: A Conversation with Shaykh Fadhlalla Haeri",
+        description: "This 1990 conversation with Shaykh Fadhlalla Haeri, in which he talks about wide-ranging topics, is as relevant today as it was when it was first conducted, for it presents Islam as the archetypal, universal, Adamic path that began when humanity rose in consciousness to recognize duality and began its journey from the relative back to Absolute Unity.",
+        coverImage: "/images/book-covers/TheConnection.jpg",
+        category: "talks-courses",
+        order: 8,
+        purchaseLinks: {
+          amazon: "1928329128",
+          kindle: "B07SD7RRBZ",
+          kobo: "https://www.kobo.com/ca/en/ebook/the-connection-between-the-absolute-and-the-relative",
+          apple: "http://books.apple.com/us/book/id1465775083",
+          other: [
+            {
+                name: "Barnes & Noble",
+                url: "https://www.barnesandnoble.com/w/the-connection-between-the-absolute-and-the-relative-shaykh-fadhlalla-haeri/1137159530",
+                geography: "US"
+            },
+            {
+                name: "Takealot",
+                url: "https://www.takealot.com/the-connection-between-the-absolute-and-the-relative-a-conversat/PLID69291862",
+                geography: "ZA",
+            },
+            {
+                name: "Waterstones",
+                url: "https://www.waterstones.com/book/the-connection-between-the-absolute-and-the-relative/shaykh-fadhlalla-haeri/9781928329121",
+                geography: "UK"
+            }
+          ]
+        },
+        samplePdfUrl: "/samples/ConnectionBetweenAbsoluteAndRelative.pdf",
+      },
+      {
+        id: "spiritual-path",
+        title: "The Spiritual Path: A Conversation Between Professor Ali A. Allawi and Shaykh Fadhlalla Haeri On His Life, Thought and Work",
+        author: "Professor Ali A. Allawi and Shaykh Fadhlalla Haeri",
+        description: "This book is a transcription of <a href=\"https://shaykhfadhlallahaeri.com/videos/pathways/#the-spiritual-path\" target=\"_blank\">extensive interviews between Professor Ali A. Allawi and Shaykh Fadhlalla Haeri</a>, conducted in South Africa during January-February of 2014.\n\nIn them, Shaykh Fadhlalla Haeri talks about his life story and the spiritual journey that he embarked on and the path he has been on ever since.\n\nProfessor Allawi's questions highlight Shaykh Haeri's life-changing encounters with Sufi Masters and other spiritual teachers and his travels and teaching with Sufi communities.\n\nDrawing out Shaykh Haeri's far-reaching views on the future of religion, Sufi orders and global spirituality, the conversations probe deeply into how one attains enlightenment and what it means to live in the light of the Absolute in a relative world. They are deeply engaging and inspiring for any serious seeker.\n\nThese interviews were transcribed by Julia Khadija Lafene and edited by Anjum Jaleel.\n\nIn order to publish it as a book, chapter headings, subheadings and footnotes have been added, many gaps within the conversations have been filled, and footnotes and a glossary have been added. ",
+        coverImage: "/images/book-covers/TheConnection.jpg",
+        category: "talks-courses",
+        order: 9,
+        publishedDate: new Date('2019-06-20'),
+        purchaseLinks: {
+          amazon: "192832911X",
+          kindle: "B07QW35SXK",
+          kobo: "https://www.kobo.com/ca/en/ebook/the-spiritual-path-1",
+          apple: "http://itunes.apple.com/us/book/id1460280842",
+          other: [
+            {
+                name: "Barnes & Noble",
+                url: "https://www.barnesandnoble.com/w/the-spiritual-path-shaykh-fadhlalla-haeri/1137402471",
+                geography: "US"
+            },
+            {
+                name: "Takealot",
+                url: "https://www.takealot.com/the-spiritual-path-a-conversation-between-professor-ali-a-allawi/PLID70326044",
+                geography: "ZA",
+            },
+            {
+                name: "Waterstones",
+                url: "https://www.waterstones.com/book/the-spiritual-path/shaykh-fadhlalla-haeri/anjum-jaleel/9781928329114",
+                geography: "UK"
+            }
+          ]
+        },
+        samplePdfUrl: "/samples/TheSpiritualPath.pdf",
       },
 ]
