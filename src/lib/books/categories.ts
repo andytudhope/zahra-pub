55f3e6baf12a9a1d9d52e4153ac9d8c69b13f9d4
andytudhope <13001517+andytudhope@users.noreply.github.com>
2025-04-23 18:20:33 +0200

    implicate ordering

--- src/lib/books/categories.ts
+++ src/lib/books/categories.ts
@@ -4,7 +4,7 @@ export const categories: Categories = {
   'islam': {
     slug: 'islam',
     name: 'Islam',
-    order: 6
+    order: 2
   },
   'quran': {
     slug: 'quran',
@@ -13,23 +13,18 @@ export const categories: Categories = {
   },
   'poetry': {
     slug: 'poetry',
-    name: 'Poetry',
-    order: 2
+    name: 'Poetic Inspirations',
+    order: 5
   },
   'sufism': {
     slug: 'sufism',
     name: 'Sufism',
     order: 3
   },
-  'practices': {
-    slug: 'practices',
-    name: 'Practices',
-    order: 4
-  },
   'talks-courses': {
     slug: 'talks-courses',
     name: 'Talks and Courses',
-    order: 5
+    order: 4
   },
   'other': {
     slug: 'other',
